637b3304f76c7c0c2615e0b584d95d7dbc911aae
Elias Galeano <galeanoeliasdavid@gmail.com>
2025-06-01 20:35:30 -0300

    feature: encapsulation

--- course.go
+++ course.go
@@ -2,12 +2,41 @@ package course
 
 import "fmt"
 
-type Course struct {
-	Name    string
-	Price   float64
-	IsFree  bool
-	UserIDs []uint
-	Classes map[uint]string
+type course struct {
+	name    string
+	price   float64
+	isFree  bool
+	userIDs []uint
+	classes map[uint]string
+}
+
+func (c *course) SetName(name string) { c.name = name} // Metodo set en GO
+func (c *course) Name() string { return c.name } // Metodo get en GO
+
+func (c *course) SetPrice(price float64) { c.price = price} // Metodo set en GO
+func (c *course) Price() float64 { return c.price } // Metodo get en GO
+
+func (c *course) SetIsFree(isFree bool) { c.isFree = isFree} // Metodo set en GO
+func (c *course) IsFree() bool { return c.isFree } // Metodo get en GO
+
+func (c *course) SetUserIDs(userIDs []uint) { c.userIDs = userIDs} // Metodo set en GO
+func (c *course) UserIDs() []uint { return c.userIDs } // Metodo get en GO
+
+func (c *course) SetClasses(classes map[uint]string) {
+	c.classes = classes
+}
+
+// Funcion constructora
+func New(name string, price float64, isFree bool) *course {
+	if price == 0 {
+		price = 30
+	}
+
+	return &course{
+		name: name,
+		price: price,
+		isFree: isFree,
+	}
 }
 
 
@@ -18,13 +47,16 @@ type Course struct {
 // }
 
 // Asi se debe hacer.
-func (c *Course) ChangePrice(price float64) {
-	c.Price = price
-}
+// Esta funcion ya no sera necesaria con los metodos "get" y "set".
+// func (c *course) changePrice(price float64) {
+// 	c.Price = price
+// }
 
-func (c Course) PrintClasses() {
+
+
+func (c course) PrintClasses() {
 	text := "Las clases son: "
-	for _, class := range c.Classes {
+	for _, class := range c.classes {
 		text += class + ", "
 	}
 
