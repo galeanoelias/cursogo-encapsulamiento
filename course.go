0b2ecf0de5233b52a8533700f2e53da84f728eb2
Elias Galeano <galeanoeliasdavid@gmail.com>
2025-06-01 15:26:02 -0300

    First Commit

---
+++ course.go
@@ -0,0 +1,34 @@
+package course
+
+import "fmt"
+
+// @RecursosCompartidos
+
+type Course struct {
+	Name    string
+	Price   float64
+	IsFree  bool
+	UserIDs []uint
+	Classes map[uint]string
+}
+
+
+//	De la siguiente manera no se actualiza el metodo por que apunta hacia una copia
+//  debo pasar un puntero para solucionar el siguiente error.
+// func (c Course) ChangePrice(price float64) {
+// 	c.Price = price
+// }
+
+// Asi se debe hacer.
+func (c *Course) ChangePrice(price float64) {
+	c.Price = price
+}
+
+func (c Course) PrintClasses() {
+	text := "Las clases son: "
+	for _, class := range c.Classes {
+		text += class + ", "
+	}
+
+	fmt.Println(text[:len(text)-2])
+}
